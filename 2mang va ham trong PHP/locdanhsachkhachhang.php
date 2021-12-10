02322ab059db9ad89492a412f3785a5257da7d7c
chieman276 <chieman2k3@gmail.com>
2021-12-10 16:08:46 +0700

    loc danh sach khack hang

--- 2mang va ham trong PHP/locdanhsachkhachhang.php
+++ 2mang va ham trong PHP/locdanhsachkhachhang.php
@@ -1,49 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<form method="GET">
-    Chọn ngày sinh từ: <input id="from" type="date" name="from" placeholder="yyyy/mm/dd" value="" />
-    đến: <input id="to" type="date" name="to" placeholder="yyyy/mm/dd" value="" />
-    <input type="submit" id="submit" value="Lọc" />;
-    <table border="1">
-    <caption>
-        <h2>Danh sách khách hàng</h2>
-    </caption>
-    <tr>
-        <th>STT</th>
-        <th>Tên</th>
-        <th>Ngày sinh</th>
-        <th>Địa chỉ</th>
-        <th>Ảnh</th>
-    </tr>
-    <?php foreach ($filteredCustomers as $index => $customer): ?>
-    <tr>
-        <td><?php echo $index + 1; ?></td>
-        <td><?php echo $customer['name']; ?></td>
-        <td><?php echo $customer['day_of_birth']; ?></td>
-        <td><?php echo $customer['address']; ?></td>
-        <td>
-            <div class="profile"><img src="<?php echo $customer['profile']; ?>" /></div>
-        </td>
-    </tr>
-    <?php endforeach; ?>
-</table>
-
-<?php
-$fromDate = null;
-$toDate = null;
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $fromDate = $_REQUEST["from"];
-    $toDate = $_REQUEST["to"];
+<style>
+table {
+    border-top-color: #ff0000;
+    border-top-color: red;
+    border-top-color: rgb(255, 0, 0);
+    width: 500px;
 }
-$filteredCustomers = searchByDate($customerList, $fromDate, $toDate);
 
+h2 {
+    background-attachment: ;
+}
+</style>
+<?php
 $customerList = [
     "1" => [
         "name" => "Mai Văn Hoàn",
@@ -71,27 +38,82 @@ $customerList = [
         "address" => "Hà Nội",
         "profile" => "images/img5.jpg"]
 ];
-function searchByDate($customers, $fromDate, $toDate)
-{
-    if (empty($fromDate) || empty($toDate)) {
-        return $customers;
-    }
-
-    $filteredCustomers = [];
-    foreach ($customers as $customer) {
-        if (strtotime($customer['day_of_birth']) < strtotime($fromDate))
-            continue;
-        if (strtotime($customer['day_of_birth']) > strtotime($toDate))
-            continue;
-        $filteredCustomers[] = $customer;
-    }
-    return $filteredCustomers;
-}
-
 ?>
 
 
-</form>
+<?php
+
+function searchByDate($customer,$fromDate,$toDate){
+    if (empty($fromDate) || empty($toDate)){
+        return $customer;
+    }
+    $filteredCustomers = [];
+    foreach ($customer as $customer){
+        if (strtotime($customer['day_of_birth']) < strtotime($fromDate))
+        continue;
+    if (strtotime($customer['day_of_birth']) > strtotime($toDate))
+    continue;
+    $filteredCustomers[] = $customer;
+    }
+    return $filteredCustomers;
+}
+?>
+<?php
+$fromDate = null;
+$toDate = null;
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if ( isset($_REQUEST["from"]) ) {
+        $fromDate = $_REQUEST["from"];
+
+    }
+    if (isset ($_REQUEST["to"])){
+        $toDate = $_REQUEST["to"];
+
+    }
+
+
+}
+$filteredCustomers = searchByDate($customerList, $fromDate, $toDate);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     
+
+<form method="GET">
+    Chọn ngày sinh từ: <input id="form" type="date" name="from" placeholder="yyyy/mm/dd" value="" />
+    Đến: <input id="to" type="date" name="to" placeholder="yyyy/mm/dd" value="" />
+    <input type="submit" id="submit" value="Lọc" />
+</form>
+
+<table border="1">
+    <caption>
+        <h2>Danh sách khách hàng</h2>
+    </caption>
+    <tr>
+        <th>STT</th>
+        <th>Tên</th>
+        <th>Ngày sinh</th>
+        <th>Địa chỉ</th>
+        <th>Ảnh</th>
+    </tr>
+    <?php foreach ($filteredCustomers as $index => $customer): ?>
+    <tr>
+        <td><?php echo $index + 1; ?></td>
+        <td><?php echo $customer['name']; ?></td>
+        <td><?php echo $customer['day_of_birth']; ?></td>
+        <td><?php echo $customer['address']; ?></td>
+        <td>
+            <div class="profile"><img src="<?php echo $customer['profile']; ?>" /></div>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
 </body>
 </html>
