f6418ddedc407ef0b65e6f51a09d7c9e626e2893
chieman276 <chieman2k3@gmail.com>
2021-12-24 14:29:33 +0700

    cài đặt sắp xếp nổi bọt

---
+++ bai 12 thuat toan sap xep/sap_xep_noi_bot.php
@@ -0,0 +1,21 @@
+<?php
+function bubbleSort($list){
+    for ($i = 0; $i < count($list)-1;$i++){
+        for($j = count($list) -1; $j > $i; $j--)
+        {
+            if ($list[$j] < $list[$j -1])
+            {
+                $t = $list[$j - 1];
+                $list[$j-1] = $list[$j];
+                $list[$j] = $t;
+            }
+        }
+    }
+    return $list;
+    
+}
+$list = [2, 3, 2, 5, 6, 1, -2, 3, 14, 12];
+foreach(bubbleSort($list) as $key => $value){
+    echo $value." ";
+}
+?>
